4cb195cf38dd4c523b3995147c5e7e48b011de1b
Prateek Srivastav <55292736+prateeksrivastava-PS@users.noreply.github.com>
2026-06-19 21:11:25 +0530

    implement update delete post operation

--- src/app.js
+++ src/app.js
@@ -2,14 +2,33 @@ const express=require("express");
 
 const app=express();
 
+app.post("/user",(req,res)=>{
+    console.log("Geting Request body",req.query)
+    res.send("Data Save  Sucessful..")
+})
+
+// It  only match get api call "/user"
+app.get("/user",(req,res)=>{
+   res.send({firstName:"Prateek",lastName:"Srivastava"})
+})
+
+app.delete("/delete/user/data",(req,res)=>{
+   console.log("Data remove from db..")
+   res.send("Data Sucessful remove...")
+})
+
 app.use("/hello",(req,res)=>{
    res.send("Hello from server");
 })
 
 
-
+// If use it match all api call lke"/test"
 app.use("/test",(req,res)=>{
    res.send("Hiw test from server");
+})
+
+app.use("/",(req,res)=>{
+   res.send("Namste Prateek");
 })
 
 
