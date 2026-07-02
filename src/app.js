7e424ea27c0217d93d051174a5b5244c5c79525d
Prateek Srivastav <55292736+prateeksrivastava-PS@users.noreply.github.com>
2026-07-02 15:47:50 +0530

    API Level data senitization

--- src/app.js
+++ src/app.js
@@ -1,124 +1,130 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB=require("./config/database");
+const connectDB = require("./config/database");
 
-const userModel= require("./models/user")
+const userModel = require("./models/user");
 
-app.use(express.json())
-app.post("/signup", async (req,res)=>{
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
 
-  console.log(req.body)
+  const user = new userModel(req.body);
 
-  const user= new userModel(req.body)
+  try {
+    await user.save();
 
-  try{
-
-   await user.save()
-    
-   res.send("Data Sucesfully save...")
-   
-  }catch(err){
-    res.status(400).send("Data not saved")
-
+    res.send("Data Sucesfully save...");
+  } catch (err) {
+    res.status(400).send(err.message);
   }
+});
 
-})
+//  Get the db data
 
-//  Get The User by emailid
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
 
-app.get("/user",async (req,res)=>{
-   
- const userEmail=req.body.emailId
- 
-  try{
-    const users=await  userModel.find({emailId:userEmail})
-    if(users.length===0){
+  try {
+    const users = await userModel.find({ emailId: userEmail });
+    if (users.length === 0) {
       res.status(404).send("User not found");
-    }else{
+    } else {
       res.send(users);
-    }  
-  }catch(err){
-    res.status(400).send("Something went wrong...")
-  
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong...");
   }
-})
+});
 
-// Getting  the  all Data
+// Get  the  all data from user
 
-app.get("/feed",async (req,res)=>{
-
-  try{
-    const allUserData=await  userModel.find({})
-    await res.status(200).send(allUserData)
-
-  }catch(err){
-     res.status(400).send("Something went wrong...")
+app.get("/feed", async (req, res) => {
+  try {
+    const allUserData = await userModel.find({});
+    await res.status(200).send(allUserData);
+  } catch (err) {
+    res.status(400).send("Something went wrong...");
   }
-
-})
+});
 
 //  finding old one  data from common data
 
-app.get("/userfindOne",async (req,res)=>{
-
-  const userId= req.body.emailId
-  try{
-    const getUserData= await userModel.findOne({emailId:userId})
-    console.log("Findta", getUserData)
-   if(getUserData.length===0){
-       res.status(404).send("User Data Not found....")
-   }else{
-           res.status(200).send(getUserData)
-   }    
-  }catch(err){
-       res.status(400).send("Something Went wrong...")
+app.get("/userfindOne", async (req, res) => {
+  const userId = req.body.emailId;
+  try {
+    const getUserData = await userModel.findOne({ emailId: userId });
+    console.log("Findta", getUserData);
+    if (getUserData.length === 0) {
+      res.status(404).send("User Data Not found....");
+    } else {
+      res.status(200).send(getUserData);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went wrong...");
   }
+});
 
-})
+// Delete  the user db
 
-// Delete
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await userModel.findByIdAndDelete({ _id: userId });
+    console.log("Deleted data", userId);
+    res.status(200).send("Data Deleted sucessfuly..");
+  } catch (err) {
+    res.status(400).send("Something went wrong..");
+  }
+});
 
-app.delete("/user",async (req,res)=>{
+// Update the user db
 
-  const userId=req.body.userId;
-try{
-  const user= await userModel.findByIdAndDelete({_id:userId})
-  console.log("Deleted data",userId)
-  res.status(200).send("Data Deleted sucessfuly..")
-}catch(err){
-res.status(400).send("Something went wrong..")
-}
-})
+app.patch("/user{/:userId}", async (req, res) => {
+  try {
+    const emailId = req.body?.emailId;
+    const userId = req.params?.userId;
+    // Copy request body
+    const data = { ...req.body };
 
-// Update 
+    // Remove emailId so it isn't updated
+    delete data.emailId;
 
-app.patch("/user",async (req,res)=>{
-  const userId=req.body.firstName
-  try{
-   
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update Not Allowed");
+    }
+
+    if (data?.skills?.length > 5) {
+      throw new Error("Skills cannot be more than 5");
+    }
+
     // await userModel.findByIdAndUpdate(userId,req.body)
-  await  userModel.findOneAndUpdate({firstName:userId},req.body,{new:true})
-   
-    res.status(200).send("Data Updated suceefuly..")
+    await userModel.findOneAndUpdate({ emailId: emailId }, req.body, {
+      returnDocument: "after",
+      runValidators: true,
+    });
 
-  }catch(err){
-    res.status(404).send("Something went wrong...")
+    res.status(200).send("Data Updated suceefuly..");
+  } catch (err) {
+    res.status(404).send(err.message);
   }
-})
-
+});
 
 connectDB()
-.then(()=>{
-  console.log("Server coneccted suceesfuly..");
-  app.listen (7777,()=>{
-    console.log("Server start listining on port 7777....")
-  })  
-
-}).catch(
-  (err)=>{
-        console.error("Database Err",err)
-  }
-)
+  .then(() => {
+    console.log("Server coneccted suceesfuly..");
+    app.listen(7777, () => {
+      console.log("Server start listining on port 7777....");
+    });
+  })
+  .catch((err) => {
+    console.error("Database Err", err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,51 @@ const mongoose= require("mongoose");
 
 const userSchema= mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        require:true,
+        minLength:2,
+        maxLength:40
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+        lowerCase:true
     },
     password:{
-        type:String
+        type:String,
+        require :true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
-    }
-})
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender Not Valid")
+
+            }
+
+        }
+    },
+  photoUrl:{
+    type:String,
+    default:"https://www.shutterstock.com/image-vector/isolated-object-avatar-dummy-symbol-260nw-1290296656.jpg"
+  },
+  about:{
+    type:String,
+    default:"This is default about  of the user!"
+  },
+  skills:{
+    type:[String]
+  }
+},{timestamps:true})
 
 module.exports= mongoose.model("User",userSchema);
 
